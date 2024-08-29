678e393b93e83c31553ef25f20b469b03d57d2e4
SaiSumanthLokinindi <saisumanthlokinindi@gmail.com>
2024-08-29 09:15:06 +0530

    29-08-24

--- Matrix/wordSearch.js
+++ Matrix/wordSearch.js
@@ -1,59 +1,26 @@
-function checkVisited(visited, index) {
-  for (let i = 0; i < visited.length; i++) {
-    if (index[0] === visited[i][0] && index[1] === visited[i][1]) return true;
-  }
-
-  return false;
-}
-
-function checkWord(board, word, p, i, j, m, n, visited) {
+function checkWord(board, word, p, i, j, m, n) {
   if (p === word.length) return true;
-  let up = false,
-    down = false,
-    left = false,
-    right = false;
-  if (
-    i - 1 >= 0 &&
-    board[i - 1][j] === word[p] &&
-    !checkVisited(visited, [i - 1, j])
-  ) {
-    console.log("up", i - 1, j);
-    visited.push([i, j]);
-    up = checkWord(board, word, p + 1, i - 1, j, m, n, visited);
-  }
 
   if (
-    i + 1 < m &&
-    board[i + 1][j] === word[p] &&
-    !checkVisited(visited, [i + 1, j])
-  ) {
-    console.log("down", i + 1, j);
-    visited.push([i, j]);
-    down = checkWord(board, word, p + 1, i + 1, j, m, n, visited);
-  }
+    i < 0 ||
+    j < 0 ||
+    i === m ||
+    j === n ||
+    board[i][j] !== word[p] ||
+    board[i][j] === "!"
+  )
+    return false;
 
-  if (
-    j - 1 >= 0 &&
-    board[i][j - 1] === word[p] &&
-    !checkVisited(visited, [i, j - 1])
-  ) {
-    console.log("left", i, j - 1, p + 1);
-    visited.push([i, j]);
-    left = checkWord(board, word, p + 1, i, j - 1, m, n, visited);
-  }
+  let c = board[i][j];
+  board[i][j] = "!";
 
-  if (
-    j + 1 < n &&
-    board[i][j + 1] === word[p] &&
-    !checkVisited(visited, [i, j + 1])
-  ) {
-    console.log("right", i, j + 1);
-    visited.push([i, j]);
-    right = checkWord(board, word, p + 1, i, j + 1, m, n, visited);
-  }
+  const up = checkWord(board, word, p + 1, i - 1, j, m, n);
+  const down = checkWord(board, word, p + 1, i + 1, j, m, n);
+  const left = checkWord(board, word, p + 1, i, j - 1, m, n);
+  const right = checkWord(board, word, p + 1, i, j + 1, m, n);
 
-  if (up || down || left || right) return true;
-  else return false;
+  board[i][j] = c;
+  return up || down || left || right;
 }
 
 /**
@@ -69,7 +36,7 @@ var exist = function (board, word) {
   for (let i = 0; i < m; i++) {
     for (let j = 0; j < n; j++) {
       if (board[i][j] === word[p]) {
-        if (checkWord(board, word, p + 1, i, j, m, n, visited)) {
+        if (checkWord(board, word, p, i, j, m, n)) {
           return true;
         }
       }
